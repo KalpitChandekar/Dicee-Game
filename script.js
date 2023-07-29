894f1690e58e4a9bf076f726e8a5069a91a55e8c
Kalpit Chandekar <kalpitchandekar1736@gmail.com>
2023-07-29 13:20:16 +0530

    add script to img2

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ var randomImageSource = "images/" + randomDiceImage;
 var image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", randomImageSource);
+
+
+//<-------Image 2------->//
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage = "dice" + randomNumber2 + ".png";
+
+var randomImageSource = "images/" + randomDiceImage;
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomImageSource);
