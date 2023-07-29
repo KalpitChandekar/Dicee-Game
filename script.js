cff8ee09ea28db3260d798bac999997ee04d4756
Kalpit Chandekar <kalpitchandekar1736@gmail.com>
2023-07-29 13:49:01 +0530

    add script to h1

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ var image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", randomImageSource);
 
-
 //<-------Image 2------->//
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
@@ -22,3 +21,13 @@ var randomImageSource = "images/" + randomDiceImage;
 var image2 = document.querySelectorAll("img")[1];
 
 image2.setAttribute("src", randomImageSource);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+} 
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = " Player 2 Wins! 🚩";
+} 
+else {
+  document.querySelector("h1").innerHTML = " Draw! ";
+}
